0c75665bb871b71d8a85cbf28f9cdf65231d83e6
John Campbell <me@johncampbell.ninja>
2014-09-22 19:03:21 +0930

    File created

---
+++ orphanSlayer.js
@@ -0,0 +1,23 @@
+/** ORPHAN SLAYER - The world's most irrelevant jQuery plugin **/
+var orphanSlayer = {
+
+	targets		: '#your-selector, .another-selector', // These are your selectors, regions where you want the orphans dealt with.
+	timer		: 1000 // Delay before running this script, in milliseconds (gives precedence to other scripts)
+
+}
+
+setTimeout(function(){
+
+	$(orphanSlayer.targets).each(function(){
+
+		orphanSlayer.content = $(this).html().split(' ');
+		orphanSlayer.last = orphanSlayer.content[orphanSlayer.content.length - 2] + '&nbsp;' + orphanSlayer.content[orphanSlayer.content.length - 1];
+		orphanSlayer.content[orphanSlayer.content.length - 2] = orphanSlayer.last;
+		orphanSlayer.content.splice((orphanSlayer.content.length - 1), 1);
+		orphanSlayer.content = orphanSlayer.content.join(' ');
+		
+		$(this).html(orphanSlayer.content);	
+
+	});
+
+}, orphanSlayer.timer );
