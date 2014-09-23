50f165b0c38f4de05bc326e840ca0abefda2a117
John Campbell <me@johncampbell.ninja>
2014-09-23 14:30:15 +0930

    Ensure tags don't get broken

--- orphanSlayer.js
+++ orphanSlayer.js
@@ -1,8 +1,10 @@
 /** ORPHAN SLAYER - The world's most irrelevant jQuery plugin **/
 var orphanSlayer = {
 
-	targets		: '#your-selector, .another-selector', // These are your selectors, regions where you want the orphans dealt with.
-	timer		: 1000 // Delay before running this script, in milliseconds (gives precedence to other scripts)
+	targets		: '#intro-banner h1, #intro-banner h3, #philosophy .cols p, #slides .text p, .part .box p, .box li, .slide h3, .slide p', // These are your selectors, regions where you want the orphans removed.
+	timer		: 1000, // Delay before running this script, in milliseconds (gives precedence to other scripts, given that this functionality is pretty irrelevant)
+	content		: '',
+	last		: ''
 
 }
 
@@ -13,10 +15,15 @@ setTimeout(function(){
 		orphanSlayer.content = $(this).html().split(' ');
 		orphanSlayer.last = orphanSlayer.content[orphanSlayer.content.length - 2] + '&nbsp;' + orphanSlayer.content[orphanSlayer.content.length - 1];
 		orphanSlayer.content[orphanSlayer.content.length - 2] = orphanSlayer.last;
-		orphanSlayer.content.splice((orphanSlayer.content.length - 1), 1);
-		orphanSlayer.content = orphanSlayer.content.join(' ');
 		
-		$(this).html(orphanSlayer.content);	
+		if (!(orphanSlayer.last.split('<').length > 1)) {
+		
+			orphanSlayer.content.splice((orphanSlayer.content.length - 1), 1);
+			orphanSlayer.content = orphanSlayer.content.join(' ');
+		
+			$(this).html(orphanSlayer.content);	
+			
+		}
 
 	});
 
